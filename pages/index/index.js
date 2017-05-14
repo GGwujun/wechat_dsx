9a18d4449072af0726d803ea83ad5ef3e1b7109e
GGwujun <876337269@qq.com>
2017-05-14 23:06:57 +0800

    1.0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,24 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var name_time="大师兄"
 Page({
   data: {
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      '../../images/lzKvwU2.jpg',
+      '../../images/slide3.jpg',
+      '../../images/timg.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    marginLeft: 0,
+    pageX: 0,//初始位置记录
+    pageY: 0,//记录Y轴的位置
+    animation: {},
+    isMoveRight: false,
+    txt:name_time
   },
   // 页面加载
   onLoad: function (options) {
@@ -145,5 +152,100 @@ Page({
   // 定位函数，移动位置到地图中心
   movetoPosition: function () {
     this.mapCtx.moveToLocation();
+  },
+  start: function (e) {
+    var pageX = e.touches[0].clientX;//记录触摸位置X方向
+    var pageY = e.touches[0].clientY;//记录触摸位置Y方向
+    var marginLeft = this.data.marginLeft;
+
+    if (Math.abs(marginLeft) == 100) {//点击回到原点
+      this.animation.translate(100).step({ duration: 800 });
+      this.setData({
+        animation: this.animation.export(),
+      })
+      var that = this;
+      setTimeout(function () {
+        that.setData({
+          marginLeft: 0
+        })
+      }, 800)
+      return;
+    }
+
+    this.setData({
+      pageX: pageX,
+      pageY: pageY
+    })
+    console.log(pageX);
+  },
+  move: function (e) {
+    var pageX = this.data.pageX;
+    var pageY = this.data.pageY;
+    var marginLeft = this.data.marginLeft;
+    var moveX = e.changedTouches[0].clientX - pageX;//记录当前移动的X轴
+    var moveY = e.changedTouches[0].clientY - pageY;//记录当前移动Y轴
+
+    if (moveX > 0) {
+      this.setData({
+        marginLeft: 0,
+      })
+      return;
+    }
+
+    if (Math.abs(marginLeft) >= 90) {//为了快速拖动不出现空白区域
+      this.setData({
+        marginLeft: -100,
+        isMoveRight: true
+      })
+      return;
+    }
+
+
+    console.log(moveX)
+    var isMoveRight = false;
+    if (Math.abs(moveX) > Math.abs(moveY) && Math.abs(moveX) > 0) { //判断是左向右滑动
+      //当marginLfet最大值时才可以向右滑动,且moveX=所设置的最大值
+      if (Math.abs(marginLeft) >= 100) {
+        moveX = -100; //因为是左滑动 所以是负
+        isMoveRight = true;
+      }
+
+      this.setData({
+        marginLeft: moveX,
+        isMoveRight: isMoveRight
+      })
+    }
+  },
+  end: function (e) {
+    var marginLeft = this.data.marginLeft;
+    var isMoveRight = this.data.isMoveRight;
+    var that = this;
+    if (!isMoveRight) {
+      if (Math.abs(marginLeft) > 50) {//超过一半时，自动到滑动到最大
+        var tx = 100 - Math.abs(marginLeft);
+        that.animation.translate(-tx).step({ duration: 500 });
+        that.setData({
+          animation: that.animation.export(),
+        })
+        setTimeout(function () {
+          that.setData({
+            marginLeft: -100,
+            isMoveRight: true
+          })
+        }, 500)
+      } else { //没有一半时，自动滑动到0
+
+        that.animation.translate(Math.abs(marginLeft)).step({ duration: 500 });
+        that.setData({
+          animation: that.animation.export(),
+        })
+        setTimeout(function () {
+          that.setData({
+            marginLeft: 0,
+            isMoveRight: false
+          })
+        }, 500)
+      }
+    }
   }
 })
